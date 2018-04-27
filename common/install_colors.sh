4ddc6bd9cabb494180875794364da7b16125516b
Rikuo Hasegawa <rikuo.hase1997@gmail.com>
2018-04-28 02:57:53 +0900

    Polish mac files

---
+++ common/install_colors.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+mkdir -p ~/.vim/colors
+cp colors/* ~/.vim/colors
--- mac_only/symlink_dotfiles.sh
+++ mac_only/symlink_dotfiles.sh
@@ -1,19 +1,16 @@
 #!/bin/bash
 
 remove_and_simlink(){
+    if [ -e ~/$1 ] || [ -h ~/$1 ]; then
+      rm ~/$1
+    fi
 
-if [ -a ~/$1 ]; then
-  rm ~/$1
-fi
-
-ln -s $(pwd)/$1 ~/$1
-
+    ln -s $(pwd)/$2 ~/$1
 }
 
 # Move respective dotfiles to their rightful places
-remove_and_simlink .vimrc
-remove_and_simlink .zshrc
-remove_and_simlink .tmux.conf
-remove_and_simlink .gitconfig
-remove_and_simlink .gitignore
-
+remove_and_simlink .vimrc ../common/.vimrc
+remove_and_simlink .zshrc .zshrc
+remove_and_simlink .tmux.conf .tmux.conf
+remove_and_simlink .gitconfig ../common/.gitconfig
+remove_and_simlink .gitignore ../common/.gitignore
