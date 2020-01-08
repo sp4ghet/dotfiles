50afc732185210be7873f881b4e0068d168d4551
sp4ghet <8775460+sp4ghet@users.noreply.github.com>
2020-01-08 19:23:47 +0000

    update dotfiles

--- mac_only/setup_mac.sh
+++ mac_only/setup_mac.sh
@@ -3,25 +3,26 @@
 set -e
 
 # Set up mac dev tools
-echo "Answer prompt, set up dev tools"
-#xcode-select --install
+# echo "Answer prompt, set up dev tools"
+# xcode-select --install
 
 # Set up kryptonite CLI tool for ssh keygen
-curl https://krypt.co/kr | sh
+#curl https://krypt.co/kr | sh
 
 # Install oh-my-zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+#sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 #npm install -g pure-prompt
 
 # Install spaceship theme
-git clone https://github.com/denysdovhan/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt"
-ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
+# git clone https://github.com/denysdovhan/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt"
+# echo ok
+# ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
 
 # Install homebrew, brew-file, and brew cask
 # brew-file gives you idempotent brew
 # brew cask gives you binaries via `brew cask install` for things like chrome
-curl -fsSL https://raw.github.com/rcmdnk/homebrew-file/install/install.sh |sh
+# curl -fsSL https://raw.github.com/rcmdnk/homebrew-file/install/install.sh |sh
 
 brew install rcmdnk/file/brew-file
 brew file set_repo
@@ -29,5 +30,5 @@ brew file set_repo
 brew file install
 
 # Install atom packages
-apm install --packages-file ../common/atom-packages.txt
+#apm install --packages-file ../common/atom-packages.txt
 
